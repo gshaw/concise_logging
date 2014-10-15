db4f4ae74bccc9f4bb8997642d5590e70bf16545
Gerry Shaw <gshaw@venturemedia.com>
2014-10-14 17:42:43 -0700

    Do not enable concise_logging in development

--- lib/concise_logging/railtie.rb
+++ lib/concise_logging/railtie.rb
@@ -1,9 +1,11 @@
 module ConciseLogging
   class Railtie < Rails::Railtie
-    initializer "railtie.configure_rails_initialization" do |app|
-      app.middleware.use ConciseLogging::LogMiddleware
-    end
+    unless Rails.env.development?
+      initializer "railtie.concise_logging" do |app|
+        app.middleware.use ConciseLogging::LogMiddleware
+      end
 
-    ConciseLogging::LogSubscriber.attach_to :action_controller
+      ConciseLogging::LogSubscriber.attach_to :action_controller
+    end
   end
 end
